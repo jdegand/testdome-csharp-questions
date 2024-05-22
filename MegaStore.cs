907c07aac0be5779ccc7707b47e37a194cfc0b4f
jdegand <70610011+jdegand@users.noreply.github.com>
2024-05-22 19:53:49 -0400

    feat: add MegaStore

---
+++ MegaStore.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class MegaStore
+{
+    public enum DiscountType
+    {
+        Standard,
+        Seasonal,
+        Weight
+    }
+
+    public static double GetDiscountedPrice(double cartWeight, double totalPrice, DiscountType discountType)
+    {
+        switch (discountType)
+        {
+            case DiscountType.Standard:
+                return totalPrice - (totalPrice * 0.06);
+            case DiscountType.Seasonal:
+                return totalPrice - (totalPrice * 0.12);
+            case DiscountType.Weight:
+                if (cartWeight > 10)
+                {
+                    return totalPrice - (totalPrice * 0.18);
+                }
+                else
+                {
+                    return totalPrice - (totalPrice * 0.06);
+                }
+            default:
+                return totalPrice - (totalPrice * 0.06);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetDiscountedPrice(12, 100, DiscountType.Weight));
+    }
+}
