11b049f468d17800d51d3f3234b33928c7ce56c0
jdegand <70610011+jdegand@users.noreply.github.com>
2024-05-22 21:55:12 -0400

    feat: add GamePlatform

---
+++ GamePlatform.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class GamePlatform
+{
+    public static double CalculateFinalSpeed(double initialSpeed, int[] inclinations)
+    {
+        double speed = initialSpeed;
+        
+        for (int i = 0; i < inclinations.Length; i++) 
+         {
+            if(inclinations[i] < 0){
+                speed += Math.Abs(inclinations[i]); 
+            } else {
+                speed -= Math.Abs(inclinations[i]); 
+                if(speed <= 0){
+                    return 0;
+                }
+            }  
+			Console.WriteLine(speed);
+         }
+        return speed;
+
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CalculateFinalSpeed(60, new int[] { 0, 30, 0, -45, 0 }));
+    }
+}
